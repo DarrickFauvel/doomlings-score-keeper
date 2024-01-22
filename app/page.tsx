59fa7f4d0827f9b58ac0a41ee7d1a413e72247b0
Darrick Fauvel <darrick.fauvel@gmail.com>
2024-01-21 19:26:45 -0500

    Create header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,3 @@
-import localFont from "next/font/local"
-
-const myFont = localFont({
-  src: "../assets/fonts/obelixpro-cyr.ttf",
-  display: "swap",
-})
-
 export default function Home() {
-  return (
-    <main>
-      <header>
-        <h1
-          className={`${myFont.className} heading text-6xl tracking-wider`}
-          data-text="DOOMLINGS">
-          DOOMLINGS
-        </h1>
-      </header>
-    </main>
-  )
+  return <main>main content</main>
 }
